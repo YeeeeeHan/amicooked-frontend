4947551c4da4970d79c68d7ae436bb5dabd8e2ea
Lim Yee Han <limyeehan@gmail.com>
2024-09-25 21:55:38 +0800

    add routing + `about` and `home` pages

---
+++ src/pages/About.tsx
@@ -0,0 +1,12 @@
+const About = () => {
+  return (
+    <div className="p-4">
+      <h1 className="text-2xl font-bold">About Us</h1>
+      <p className="mt-2 text-lg">
+        This is the about page. Here you can add information about your project.
+      </p>
+    </div>
+  )
+}
+
+export default About

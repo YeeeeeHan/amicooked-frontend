27c32cb5963cc025f610e248a9db79d954d25fce
Lim Yee Han <limyeehan@gmail.com>
2024-10-08 22:25:27 +0800

    add breadcrumbs

---
+++ src/components/common/BreadCrumb.tsx
@@ -0,0 +1,43 @@
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from '@/components/ui/breadcrumb'
+
+type BreadcrumbItem = {
+  label: string
+  href?: string
+}
+
+type BreadcrumbPage = {
+  label: string
+}
+
+interface BreadcrumbProps {
+  links: BreadcrumbItem[]
+  page: BreadcrumbPage
+}
+
+export function CustomBreadcrumb({ links, page }: BreadcrumbProps) {
+  return (
+    <Breadcrumb>
+      <BreadcrumbList className="stroke-primary-strong text-primary-strong">
+        {links.map((item, index) => (
+          <div className="inline-flex items-center gap-1.5">
+            <BreadcrumbItem key={index}>
+              <BreadcrumbLink href={item.href}>{item.label}</BreadcrumbLink>
+            </BreadcrumbItem>
+            <BreadcrumbSeparator />
+          </div>
+        ))}
+
+        <BreadcrumbItem>
+          <BreadcrumbPage>{page.label}</BreadcrumbPage>
+        </BreadcrumbItem>
+      </BreadcrumbList>
+    </Breadcrumb>
+  )
+}
